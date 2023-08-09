a18623b59f85caffeac30fba084a3d6d8930104f
AviChakravarti14 <139204734+AviChakravarti14@users.noreply.github.com>
2023-08-09 17:04:03 +0530

    Add files via upload

---
+++ src/app/cpipe.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'cpipe'
+})
+export class CpipePipe implements PipeTransform {
+
+  transform(num1:number): number {
+    return (num1+(0.1*num1)+(0.05*num1)+(0.03*num1))
+  }
+
+}
---
+++ src/app/pipeex/pipeex.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipeex',
+  templateUrl: './pipeex.component.html',
+  styleUrls: ['./pipeex.component.css']
+})
+export class PipeexComponent {
+  basic:number=25000;
+
+}
